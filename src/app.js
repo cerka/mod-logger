1932216efd75ec0aa8d7944a44978cb942dbfd87
duhl <agoichon@gmail.com>
2018-07-20 09:18:01 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const conf = require('mod-config').logger;
+const Bunyan = require('bunyan');
+
+const logger_defaults = {name:'logger',level:'trace'};
+class Logger extends Bunyan {
+    constructor () {
+        super();    
+    }
+
+    static getInstance() {
+        if (!this._instance) {
+            if (conf) {
+                this._instance = Logger.createLogger(conf);
+                this._instance.trace('logger configured');
+            } else {
+                this._instance = Logger.createLogger(logger_defaults);
+                this._instance.trace('logger not configured, see https://github.com/trentm/node-bunyan/blob/master/README.md');
+            }
+        }
+        return this._instance;
+    }
+}
+
+module.exports = Logger.getInstance();
