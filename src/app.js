6391d53a228226455205d29e5ace4c28d2cbd811
Cerka <agoichon@gmail.com>
2021-03-09 22:39:30 +0100

    remove commas

--- src/app.js
+++ src/app.js
@@ -1,26 +1,26 @@
-const conf = require('mod-config').logger;
-const Bunyan = require('bunyan');
+const conf = require('mod-config').logger
+const Bunyan = require('bunyan')
 
-const logger_defaults = {name:'logger',level:'trace'};
+const logger_defaults = {name:'logger',level:'trace'}
 
 class Logger extends Bunyan {
     constructor () {
-        super();        
+        super()        
     }
 
     static getInstance() {
         if (!this._instance) {
             if (conf) {
-                this._instance = Logger.createLogger(conf);
-                this._instance.trace('logger configured');
+                this._instance = Logger.createLogger(conf)
+                this._instance.trace('logger configured')
             } else {
-                this._instance = Logger.createLogger(logger_defaults);
-                this._instance.trace('logger not configured, see https://github.com/trentm/node-bunyan/blob/master/README.md');
+                this._instance = Logger.createLogger(logger_defaults)
+                this._instance.trace('logger not configured, see https://github.com/trentm/node-bunyan/blob/master/README.md')
             }
             this._instance.log = this._instance.info
         }
-        return this._instance;
+        return this._instance
     }
 }
 
-module.exports = Logger.getInstance();
+module.exports = Logger.getInstance()
